c424906be707f71b13114c59cbe90e69862e99a0
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-06-16 10:14:46 +0200

    WIP

--- native/commonizer/src/org/jetbrains/kotlin/commonizer/facade.kt
+++ native/commonizer/src/org/jetbrains/kotlin/commonizer/facade.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.commonizer.ResultsConsumer.Status
 import org.jetbrains.kotlin.commonizer.tree.CirTreeRoot
 import org.jetbrains.kotlin.commonizer.tree.deserializeCirTree
 import org.jetbrains.kotlin.commonizer.utils.progress
+import org.jetbrains.kotlin.storage.NullableLazyValue
 import java.util.Comparator.comparing
 
 fun runCommonization(parameters: CommonizerParameters) {
@@ -16,39 +17,62 @@ fun runCommonization(parameters: CommonizerParameters) {
         parameters.resultsConsumer.allConsumed(parameters, Status.NOTHING_TO_DO)
         return
     }
-    runCommonization(parameters, parameters.outputTargets.map(::enqueueTarget))
+    val pendingCommonizerTargets = parameters.outputTargets.map { target -> PendingCommonizerTarget(parameters, target) }
+    val pendingCommonizerJobs = buildCommonizerJobQueue(parameters, pendingCommonizerTargets)
+    pendingCommonizerJobs.forEach(PendingCommonizationJob::invoke)
     parameters.resultsConsumer.allConsumed(parameters, Status.DONE)
 }
 
-internal fun runCommonization(parameters: CommonizerParameters, queue: List<EnqueuedCommonizerTarget>) {
-    val enqueuedTargets = queue.associate { enqueuedTarget -> enqueuedTarget.target as CommonizerTarget to enqueuedTarget.producer }
-
-    val providedTargets = parameters.targetProviders.targets.associateWith { target ->
-        parameters.storageManager.createNullableLazyValue {
-            parameters.logger.progress(target, "Deserialized declarations") {
-                deserializeCirTree(parameters, target)
+internal fun buildCommonizerJobQueue(
+    parameters: CommonizerParameters,
+    queue: List<PendingCommonizerTarget>
+): List<PendingCommonizationJob> {
+    // All targets that can just be de-serialized and do not need any commonization to happen first
+    val providedTargets: TargetDependent<NullableLazyValue<CirTreeRoot>> =
+        EagerTargetDependent(parameters.targetProviders.targets) { target ->
+            parameters.storageManager.createNullableLazyValue {
+                parameters.logger.progress(target, "Deserialized declarations") {
+                    deserializeCirTree(parameters, target)
+                }
             }
         }
-    }
 
-    val context = object : EnqueuedCommonizerTarget.AbstractContext(parameters) {
-        private val lazyEnqueuedTargets = enqueuedTargets
-            .mapValues { (_, producer) -> parameters.storageManager.createNullableLazyValue { producer.invoke(this) } }
+    // Targets that are enqueued for commonization and pending
+    val pendingTargets: MutableMap<CommonizerTarget, NullableLazyValue<CirTreeRoot>> = mutableMapOf()
 
-        override fun requireCirTree(target: CommonizerTarget): CirTreeRoot? {
-            return if (target in providedTargets) providedTargets.getValue(target).invoke()
-            else lazyEnqueuedTargets.getValue(target).invoke()
+    fun enqueueTargetIfNecessary(target: PendingCommonizerTarget): PendingCommonizationJob? {
+        if (pendingTargets.containsKey(target.outputTarget)) return null
+
+        // Ensure dependencies are enqueued first
+        target.inputTargets.forEach { inputTarget ->
+            queue.filter { it.outputTarget == inputTarget }.forEach { enqueueTargetIfNecessary(it) }
         }
 
-        fun createTasks(): List<() -> Unit> {
-            return lazyEnqueuedTargets.keys
-                .sortedWith(comparing<CommonizerTarget?, Int?> { it.allLeaves().size }.then(comparing { it.konanTargets.size }))
-                .map(lazyEnqueuedTargets::getValue)
-                .map { { it.invoke() } }
+        // Enqueue this target
+        val inputDeclarations: TargetDependent<NullableLazyValue<CirTreeRoot>> = EagerTargetDependent(target.inputTargets) { inputTarget ->
+            pendingTargets[inputTarget] ?: providedTargets.getOrNull(inputTarget)
+            ?: throw IllegalStateException("Missing inputTarget $inputTarget")
         }
+
+        val context = PendingCommonizerTarget.Context(parameters, inputDeclarations)
+        val lazyDeclarations = parameters.storageManager.createNullableLazyValue { target.producer.invoke(context) }
+        pendingTargets[target.outputTarget] = lazyDeclarations
+        return PendingCommonizationJob(target.outputTarget, lazyDeclarations)
     }
 
-    runCommonization(context.createTasks())
+    // Enqueue all targets
+    return queue
+        .sortedWith(PendingCommonizerTargetComparator)
+        .mapNotNull { enqueuedCommonizerTarget -> enqueueTargetIfNecessary(enqueuedCommonizerTarget) }
 }
 
-private fun runCommonization(tasks: List<Function0<*>>) = tasks.forEach { it.invoke() }
+// TODO NOW: Test
+internal val PendingCommonizerTargetComparator = comparing<PendingCommonizerTarget, Int> { it.inputTargets.size }
+    .then(comparing { it.outputTarget.allLeaves().size })
+
+internal class PendingCommonizationJob(
+    val outputTarget: SharedCommonizerTarget,
+    private val declarations: NullableLazyValue<CirTreeRoot>
+) {
+    operator fun invoke() = declarations.invoke()
+}
